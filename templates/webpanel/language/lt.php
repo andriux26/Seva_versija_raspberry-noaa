1faafafbc5c22e5416ec9138496da9a833a58090
andriux26 <andriux26@gmail.com>
2023-08-21 10:54:35 +0300

    Create lt.php

---
+++ templates/webpanel/language/lt.php
@@ -0,0 +1,15 @@
+<?php
+$lang = array(
+  "passes" => "Laikai",
+  "satellite" => "Pavidovas",
+  "images" => "Nuotraukos",
+  "pass_start" => "Pradzia",
+  "pass_end" => "Pabaiga",
+  "max_elev" => "Max aukstis",
+  "elev" => "Aukstis",
+  "page" => "Puslapis",
+  "of" => "Is",
+  "prev" => "Atgal",
+  "next" => "Kitas",
+);
+?>
